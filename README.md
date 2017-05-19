c0d98f6a6240946535af6afd0684045fdaaaa088
Michael Tambornino <michael.tambornino@gmail.com>
2017-05-18 22:43:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#[Project Euler](https://projecteuler.net)
+Named for one of the great mathematicians in history, Project Euler is a collection of problems designed to make the solver think algorithmically. I am tracking my progress by saving solutions in this repo.
+
+## Example
+![screen shot 2017-05-18 at 10 35 54 pm](https://cloud.githubusercontent.com/assets/23462252/26232336/350beff8-3c1b-11e7-9e27-2f1071b39468.png)
---
+++ problems/1.js
@@ -0,0 +1,26 @@
+// https://projecteuler.net/problem=1
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+// The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+const multiSum = (x, y, top) => {
+  let sum = 0, i = 0;
+  for (let i = 0; i < top; i++) {
+    i%x === 0 ? sum += i : i%y === 0 ? sum += i : sum
+    // if (i%x === 0) {
+    //   sum += i
+    // } else
+    // if (i%y === 0) {
+    //   sum += i
+    // }
+  }
+  return sum
+};
+
+// test
+console.log("23 should equal:", multiSum(3, 5, 10))
+
+// solution
+console.log("solution:", multiSum(3, 5, 1000))
+// 233168
