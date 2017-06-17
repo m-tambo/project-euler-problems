12b9b45147d92be4f0f704fb4e9733538e4bb5b2
Michael Tambornino <michael.tambornino@gmail.com>
2017-06-17 11:40:01 -0500

    palindrome check works

--- problems/4_largest-palindrome-product.js
+++ problems/4_largest-palindrome-product.js
@@ -3,14 +3,23 @@
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
 const isNumberAPalindrome = (number) => {
-
+  let arrayOfDigits = number.toString().split('')
+  for (let i = 0; i < arrayOfDigits.length; i++) {
+    if (arrayOfDigits[i] !== arrayOfDigits[arrayOfDigits.length - 1 - i]) {
+      return false
+    }
+  }
+  return true
 }
 
 const findLargestPalindromeProduct = (upperLimit) => {
 
 }
 
-// test
+// tests
+console.log(`${isNumberAPalindrome(123321)} should be true`)
+console.log(`${isNumberAPalindrome(1234321)} should be true`)
+console.log(`${isNumberAPalindrome(123421)} should be false`)
 console.log(`${findLargestPalindromeProduct(100)} should be 9009`)
 
 // solution
